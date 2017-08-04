e3deaedf31280ecf5ec181b462053408c51f5228
Christopher Fujino <christopherfujino@gmail.com>
2017-08-04 14:47:03 -0700

    created hello world component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-// Developer TODO: Load in App component and render to the DOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './Home.js';
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
